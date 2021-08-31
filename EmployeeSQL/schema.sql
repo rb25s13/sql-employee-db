e5beef7ac5bc861e901eaede08e9b2d4012f6be1
rb25s13 <rb25s13.dwc@gmail.com>
2021-08-31 14:13:32 -0500

    updated schema, queries, readme and erd

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,42 +1,47 @@
-DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS departments cascade;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employees cascade;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
 
 CREATE TABLE departments (
-  dept_no character varying(5) NOT NULL,
-  dept_name character varying(30) NOT NULL
-);
-
-CREATE TABLE dept_emp (
-  emp_no integer NOT NULL,
-  dept_no character varying(10) NOT NULL
-);
-
-CREATE TABLE dept_manager (
-  dept_no character varying(10) NOT NULL,
-  emp_no integer NOT NULL
-);
-
-CREATE TABLE employees (
-  emp_no integer NOT NULL,
-  emp_title_id character varying(6) NOT NULL,
-  birth_date character varying(10) NOT NULL,
-  first_name character varying(20) NOT NULL,
-  last_name character varying(20) NOT NULL,
-  sex character varying(10) NOT NULL,
-  hire_date character varying(10) NOT NULL
-);
-
-CREATE TABLE salaries (
-  emp_no integer NOT NULL,
-  salary integer NOT NULL
+  dept_no varchar(4) primary key,
+  dept_name varchar(18)
 );
 
 CREATE TABLE titles (
-  title_id character varying(10) NOT NULL,
-  title character varying(20) NOT NULL	
+  title_id varchar(5) primary key,
+  title varchar(20)
 );
+
+CREATE TABLE employees (
+  emp_no serial primary key not null,
+  emp_title_id varchar(5),
+  birth_date varchar(10),
+  first_name varchar(20),
+  last_name varchar(20),
+  sex char(1),
+  hire_date varchar(10),
+  foreign key (emp_title_id) references titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+  emp_no serial,
+  dept_no varchar(4)
+);
+
+CREATE TABLE dept_manager (
+  dept_no varchar(4),
+  emp_no serial primary key not null,
+  foreign key (dept_no) references departments(dept_no),
+  foreign key (emp_no) references employees(emp_no)
+);
+
+CREATE TABLE salaries (
+  emp_no serial primary key not null,
+  salary integer,
+  foreign key (emp_no) references employees(emp_no)
+);
+
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# sql-challenge
+<h1 align="center">Employee Database: A Mystery in Two Parts</h1>
+ 
+ 
+
+<p align="center"><img src='erd.png'></p>
+
+
+<h2 align="center">Design the tables to hold data in the CSVs, import the CSVs into a SQL database, and answer questions about the data.</h2>
+			
+							
+### Contents:
+
+        /data - csv files with employee data from the 1980s and 1990s
+        /EmployeeSQL - SQL files for db queries and ipynb file for visualizations
+
+### Questions:
+	
+1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+2. List first name, last name, and hire date for employees who were hired in 1986.
+
+3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
